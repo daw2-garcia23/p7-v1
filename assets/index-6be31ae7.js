07f2645eb27b7c283318996beb3a0f7c4818e17a
daw2-garcia23 <garciamanjonjoel@fpllefia.com>
2023-01-26 19:52:35 +0100

    Updates

--- assets/index-6be31ae7.js
+++ assets/index-6be31ae7.js
@@ -7,7 +7,7 @@
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${s}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Pacual!!</h1>
+    <h1>Hello Caracola!!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
